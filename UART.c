71253081091fd2afa64fab96c7c430d0b1464876
Yeni Choi <ych227@uclive.ac.nz>
2024-05-10 15:16:50 +1200

    altitude and yaw controls working, moving onto UART

---
+++ UART.c
@@ -0,0 +1,22 @@
+/*
+ * UART.c
+ *
+ *  Created on: 6/05/2024
+ *      Author: ych227
+ */
+
+
+#include "UART.h"
+
+#define SYSTICK_RATE_HZ 100
+#define SLOWTICK_RATE_HZ 4
+#define MAX_STR_LEN 16
+//---USB Serial comms: UART0, Rx:PA0 , Tx:PA1
+#define BAUD_RATE 9600
+#define UART_USB_BASE           UART0_BASE
+#define UART_USB_PERIPH_UART    SYSCTL_PERIPH_UART0
+#define UART_USB_PERIPH_GPIO    SYSCTL_PERIPH_GPIOA
+#define UART_USB_GPIO_BASE      GPIO_PORTA_BASE
+#define UART_USB_GPIO_PIN_RX    GPIO_PIN_0
+#define UART_USB_GPIO_PIN_TX    GPIO_PIN_1
+#define UART_USB_GPIO_PINS      UART_USB_GPIO_PIN_RX | UART_USB_GPIO_PIN_TX
